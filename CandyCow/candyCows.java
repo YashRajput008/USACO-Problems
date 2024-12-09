60bbac29e6b400dbb90b8d6305ca1a4fc7338315
Yash Rajput <to.yashrajput@gmail.com>
2024-12-08 20:08:23 -0500

    Finished Cow Addition Method

--- CandyCow/candyCows.java
+++ CandyCow/candyCows.java
@@ -8,33 +8,43 @@ public class candyCows {
     int cows = askInput("Cow", 0, cowHeight);
     int candies = askInput("Candy", 0, candyHeight);
 
-    int remainingCandyHeight=candyHeight.get(0);
 
     // Running Program 
     for (int i = 0; i < cows; i++) {
+
       System.out.println("Cow " + (i+1) + " begins to eat the candy");
+      int remainingCandyHeight=candyHeight.get(0);
+
       if (remainingCandyHeight < cowHeight.get(i)){
         for (int j = 0; j < candies; j++) {
-          remainingCandyHeight = candyHeight.get(j) - cowHeight.get(i);
-          remainingCandyHeight = Math.max(0,remainingCandyHeight);
+          amountToBeDeducted = cowHeight.get(i) - candyHeight.get(j);
+          remainingCandyHeight = remainingCandyHeight - amountToBeDeducted;
+          remainingCandyHeight = Math.max(0, remainingCandyHeight);
           candyHeight.set(j, remainingCandyHeight);
-          cowHeight.set(i, remainingCandyHeight+cowHeight.get(i));
-          System.out.println("Candy # " + (j+1) + " is now " + candyHeight.get(j));
+          cowHeight.get(i) = cowHeight.get(i) + amountToBeDeducted;
         }
       } else {
         if (i == 0){
-          for (int j = 0; j < candies; j++) {
-            remainingCandyHeight = candyHeight.get(j) - cowHeight.get(i);
-            remainingCandyHeight = Math.max(0,remainingCandyHeight);
-            candyHeight.set(j, remainingCandyHeight);
-            cowHeight.set(i, remainingCandyHeight+cowHeight.get(i));
-            System.out.println("Candy # " + (j+1) + " is now " + candyHeight.get(j));
+          if (remainingCandyHeight > cowHeight.get(i)){
+            for (int j = 0; j < candies; j++) {
+              remainingCandyHeight = candyHeight.get(j) - cowHeight.get(i);
+              remainingCandyHeight = Math.max(0,remainingCandyHeight);
+              candyHeight.set(j, remainingCandyHeight);
+
+              cowHeight.get(i) = cowHeight.get(i) + Math.abs();
+
+              System.out.println("Candy # " + (j+1) + " is now " + candyHeight.get(j));
+            }
+          } else {
+            continue;
           }
         } else {
           continue;   
         }
       }
     }
+    System.out.println(cowHeight);
+    System.out.println(candyHeight);
   }
   public static int askInput(String name, int varName,  ArrayList<Integer> arrayName){
     System.out.print("Please Input # of " + name + " : ");
@@ -46,7 +56,7 @@ public class candyCows {
       int height = sc.nextInt();
       arrayName.add(height); 
     }
-    
+
     System.out.println(arrayName);
     return varName;
   }
